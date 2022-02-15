d4368b491281f3b78175b5e525daff6654b25f87
Axilotl17 <axilotl17@gmail.com>
2022-02-15 15:07:39 -0800

    please

---
+++ projects/squwordle/howto.js
@@ -0,0 +1,33 @@
+var letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g"
+]
+function  isInputWord() {
+    demoInput = document.getElementById("demoInput");
+    demoLabel = document.getElementById("demoLabel");
+    demoInput.addEventListener("change", function () {
+        rightLetters = true
+        if (isWord(demoInput.value)) {
+            for (let i = 0; i < demoInput.value.length; i++) {
+                if (letters.includes(demoInput.value.charAt(i)) === false) {
+                    rightLetters = false
+                }   
+            }
+            if (rightLetters) {
+                demoLabel.innerHTML = "Congrats, that word works!"
+                demoLabel.style.color = "green"
+            } else {
+                demoLabel.innerHTML = "Uh oh, you can't use some of those letters!"
+                demoLabel.style.color = "red"
+            }
+        } else {
+            demoLabel.innerHTML = "Not a real word!"
+            demoLabel.style.color = "red"
+        }
+    });
+}
